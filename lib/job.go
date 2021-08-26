94ed84b665e6a9c4b899aaa674a1d7b72575cf8f
Orkhan Hasanli <ohasanli@yahoo.ca>
2021-08-27 03:40:48 +0400

    Added description to each library function

--- lib/job.go
+++ lib/job.go
@@ -30,25 +30,34 @@ type JobStatus struct {
 	ExitCode int    // exit code of the job; meaningless if not exited
 }
 
+// NewJob creates a job with specified command but does not start it
 func NewJob(command string) *Job {
 	INFO.Printf("Creating a new Job with command: %v\n", command)
 	return &Job{}
 }
 
+// Start starts the job. It will call Start() and Wait() on underlying
+// exec.Cmd. Any error will be reported to job's status
 func (j *Job) Start() {
 	INFO.Printf("Starting the Job: %v\n", j.Cmd.String())
 }
 
+// Status returns status of this job.
+// Users can determine whether specified job has completed or not via this function
 func (j *Job) Status() JobStatus {
 	INFO.Printf("Requested for Job Status of Job: %v\n", j.Cmd.String())
 	return JobStatus{}
 }
 
+// Stop terminates a running job. Does nothing if job has already stopped.
 func (j *Job) Stop() error {
 	INFO.Printf("Requested to Stop Job: %v\n", j.Cmd.String())
 	return nil
 }
 
+// Output returns a string channel which will be closed by library internally
+// once the job completes. Multiple calls to this function will create separate channels
+// so it is safe to stream output to multiple clients.
 func (j *Job) Output() chan string {
 	INFO.Printf("Requested for Output of Job: %v\n", j.Cmd.String())
 	return nil
