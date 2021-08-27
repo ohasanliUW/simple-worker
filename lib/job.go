43eb9699fe8a09987c749b3eff1a2734c4087c1c
Orkhan Hasanli <ohasanli@yahoo.ca>
2021-08-27 15:57:12 +0400

    Implemented Start, Status and Stop. Added some test cases for each

--- lib/job.go
+++ lib/job.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"sync"
+	"syscall"
 )
 
 var INFO *log.Logger
@@ -17,9 +18,9 @@ type Job struct {
 	Cmd *exec.Cmd
 	Pid int
 
-	*sync.Mutex // to protect some of data from simultaneous reads and writes
-	//status      JobStatus // current status of this job
-	//doneC       chan struct{}
+	*sync.Mutex           // to protect some of data from simultaneous reads and writes
+	status      JobStatus // current status of this job
+	doneC       chan struct{}
 
 	// TODO: Add output Streamer
 }
@@ -30,29 +31,104 @@ type JobStatus struct {
 	ExitCode int    // exit code of the job; meaningless if not exited
 }
 
+func (js JobStatus) String() string {
+	return js.Status
+}
+
 // NewJob creates a job with specified command but does not start it
 func NewJob(command string) *Job {
 	INFO.Printf("Creating a new Job with command: %v\n", command)
-	return &Job{}
+
+	args := []string{"/bin/sh", "-c", command}
+
+	job := &Job{
+		Cmd:   exec.Command(args[0], args[1:]...),
+		Mutex: &sync.Mutex{},
+		doneC: make(chan struct{}),
+	}
+
+	return job
 }
 
 // Start starts the job. It will call Start() and Wait() on underlying
 // exec.Cmd. Any error will be reported to job's status
 func (j *Job) Start() {
 	INFO.Printf("Starting the Job: %v\n", j.Cmd.String())
+
+	if j.Cmd.Process != nil {
+		// This job has already started, do nothing
+		return
+	}
+
+	go func() {
+		var err error
+		defer func() {
+			j.Lock()
+			j.status.Exited = true
+
+			if j.Cmd.ProcessState != nil {
+				j.status.Status = j.Cmd.ProcessState.String()
+				j.status.ExitCode = j.Cmd.ProcessState.ExitCode()
+			} else {
+				j.status.Status = "Unknown Error, could not start the process"
+				j.status.ExitCode = -1
+			}
+
+			j.Unlock()
+			// close the channel to indicate that Job has finished
+			close(j.doneC)
+		}()
+
+		// following creates a process group, so that killing parent would kill its child processes
+		j.Cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+		// TODO: change Cmd.Stdout and Cmd.Stderr to write all of the output into a file
+
+		err = j.Cmd.Start()
+		if err != nil {
+			INFO.Println("Start() failed:", err)
+			return
+		}
+
+		j.Lock()
+		j.status.Status = "running"
+		j.status.Exited = false
+		j.Pid = j.Cmd.Process.Pid
+		j.Unlock()
+
+		INFO.Println("Successfully started job:", j.Pid)
+
+		err = j.Cmd.Wait()
+		if err != nil {
+			INFO.Println("Wait() returned with an error:", err)
+			return
+		}
+	}()
 }
 
 // Status returns status of this job.
 // Users can determine whether specified job has completed or not via this function
 func (j *Job) Status() JobStatus {
 	INFO.Printf("Requested for Job Status of Job: %v\n", j.Cmd.String())
-	return JobStatus{}
+	return j.status
 }
 
 // Stop terminates a running job. Does nothing if job has already stopped.
 func (j *Job) Stop() error {
 	INFO.Printf("Requested to Stop Job: %v\n", j.Cmd.String())
-	return nil
+	pgid, err := syscall.Getpgid(j.Pid)
+	if err != nil {
+		INFO.Printf("Unable to get group id for process %v\n", j.Pid)
+		return err
+	}
+
+	err = syscall.Kill(-pgid, syscall.SIGTERM)
+	if err != nil {
+		INFO.Printf("Unable to kill process group")
+		return err
+	}
+
+	return err
 }
 
 // Output returns a string channel which will be closed by library internally
@@ -61,4 +137,9 @@ func (j *Job) Stop() error {
 func (j *Job) Output() chan string {
 	INFO.Printf("Requested for Output of Job: %v\n", j.Cmd.String())
 	return nil
+}
+
+// Wait blocks until Job is complete.
+func (j *Job) Wait() {
+	<-j.doneC
 }
--- lib/job_test.go
+++ lib/job_test.go
@@ -2,6 +2,7 @@ package job
 
 import (
 	"testing"
+	"time"
 
 	"gopkg.in/check.v1"
 )
@@ -12,21 +13,29 @@ var _ = check.Suite(&JobSuite{})
 
 func TestLib(t *testing.T) { check.TestingT(t) }
 
-func (s *JobSuite) TestStart(c *check.C) {
+func (s *JobSuite) TestGoodStart(c *check.C) {
 	// Positive case
 	good_command := "echo Hello World; sleep 2; echo Bye!!!"
 
 	good_job := NewJob(good_command)
 	c.Assert(good_job, check.NotNil)
 
-	// TODO: Call Start()
-	// TODO: check Status to make sure no errors
-	// TODO: check PID > 0
+	good_job.Start()
 
-	// Negative case
-	bad_command := "maluba 123"
-	bad_job := NewJob(bad_command)
-	c.Assert(bad_job, check.NotNil)
+	good_job.Wait()
+	c.Assert(good_job.Pid, check.Not(check.Equals), 0)
+	good_status := good_job.Status()
+	INFO.Printf("TEST: %v\n", good_status)
+	c.Assert(good_status.Exited, check.Equals, true)
+	c.Assert(good_status.ExitCode, check.Equals, 0)
+
+	// // Negative case
+	// bad_command := "maluba 123"
+	// bad_job := NewJob(bad_command)
+	// c.Assert(bad_job, check.NotNil)
+	// bad_job.Start()
+
+	// bad_job.Wait()
 
 	// TODO: Call Start()
 	// TODO: check Status make sure it errored
@@ -34,4 +43,38 @@ func (s *JobSuite) TestStart(c *check.C) {
 
 }
 
-// TODO: Add some more test cases
+func (s *JobSuite) TestBadStart(c *check.C) {
+	// Negative case
+	bad_command := "maluba 123"
+	bad_job := NewJob(bad_command)
+	c.Assert(bad_job, check.NotNil)
+	bad_job.Start()
+	bad_job.Wait()
+	c.Assert(bad_job.status.Exited, check.Equals, true)
+
+	INFO.Printf("LIB-TEST: Exit reason: %v\n", bad_job.Status().String())
+
+	// TODO: Call Start()
+	// TODO: check Status make sure it errored
+	// TODO: check exit code
+
+}
+
+func (s *JobSuite) TestStop(c *check.C) {
+	job := NewJob("sleep 100")
+	c.Assert(job, check.NotNil)
+
+	job.Start()
+	//wait for few milliseconds then stop the job.
+	time.Sleep(time.Millisecond * 100)
+
+	c.Assert(job.Pid, check.Not(check.Equals), 0)
+	c.Assert(job.Status().Exited, check.Equals, false)
+
+	err := job.Stop()
+	job.Wait()
+	c.Assert(err, check.IsNil)
+	c.Assert(job.Status().Exited, check.Equals, true)
+	INFO.Printf("LIB-TEST: Exit reason: %v\n", job.Status().String())
+	INFO.Println(job.Status().ExitCode)
+}
